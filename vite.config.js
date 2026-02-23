b3da28170b2ec2b1940e726295d64c7346419627
MohammedSaif29 <2400031209@kluniversity.in>
2026-02-23 16:05:41 +0530

    Update project files and add docs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Fullstack_Frontend_part/",
+  base: "./",
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
